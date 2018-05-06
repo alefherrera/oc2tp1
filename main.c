7ae8b7e91d150451e8af9ecad924f091d1cd06ad
Alejandro Herrera <alef.herrera@gmail.com>
2018-05-06 19:05:55 -0300

    initial commit

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Ale
+ *
+ * Created on 6 de mayo de 2018, 18:47
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Hello, World!");
+    return (EXIT_SUCCESS);
+}
+
