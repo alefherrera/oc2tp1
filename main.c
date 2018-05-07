51a79b4f70a6ae132f39be1a33a74b621b8e68d2
andiblas <andy_blas@hotmail.com>
2018-05-06 23:10:07 -0300

    implementacion usando punteros de punteros

--- main.c
+++ main.c
@@ -15,42 +15,45 @@
 #include <stdlib.h>
 
 struct nodo_abb {
-    int valor;
-    int cantidad;
     struct nodo_abb *izq;
     struct nodo_abb *der;
+    int valor;
+    int cantidad;
 };
 
-struct nodo_abb* agregar_abb(struct nodo_abb *arbol, int val);
+struct nodo_abb* agregar_abb(struct nodo_abb **arbol, int val);
 void borrar_abb(struct nodo_abb *a);
 void mostrar_abb(struct nodo_abb *a);
 
+struct nodo_abb *root;
 /*
  * 
  */
 int main(int argc, char** argv) {
     printf("Hello, World!");
-    struct nodo_abb *arbol = agregar_abb(arbol, 1);
-    struct nodo_abb *a = agregar_abb(arbol, 2);
-    struct nodo_abb *b = agregar_abb(arbol, 3);
+    struct nodo_abb *a = agregar_abb(&root, 1);
+    struct nodo_abb *b = agregar_abb(&root, 2);
+    struct nodo_abb *c = agregar_abb(&root, 3);
     return (EXIT_SUCCESS);
 }
 
-struct nodo_abb* agregar_abb(struct nodo_abb *arbol, int val) {
-    if (arbol == NULL) {
-        arbol = (struct nodo_abb*) malloc(sizeof (struct nodo_abb));
-        arbol->valor = val;
-        arbol->cantidad = 1;
+struct nodo_abb* agregar_abb(struct nodo_abb** arbol, int val) {
+    if ((*arbol) == NULL) {
+        (*arbol) = malloc(sizeof (struct nodo_abb));
+        (*arbol)->valor = val;
+        (*arbol)->cantidad = 1;
+        (*arbol)->der = NULL;
+        (*arbol)->izq = NULL;
         return arbol;
     } else {
-        int valor = arbol->valor;
+        int valor = (*arbol)->valor;
         if (val == valor) {
-            arbol->cantidad++;
+            (*arbol)->cantidad++;
             return arbol;
         } else if (val < valor) {
-            return agregar_abb(arbol->izq, val);
+            return agregar_abb(&(*arbol)->izq, val);
         } else {
-            return agregar_abb(arbol->der, val);
+            return agregar_abb(&(*arbol)->der, val);
         }
     }
 }
