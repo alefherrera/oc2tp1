3c7edc20d2036676521481ab2b9f8660ea82ec75
andiblas <andy_blas@hotmail.com>
2018-05-07 20:48:13 -0300

    modificaciones sobre la funcion Agregar

--- main.c
+++ main.c
@@ -31,12 +31,12 @@ struct nodo_abb *root;
  * 
  */
 int main(int argc, char** argv) {
-    agregar_abb(&root, 3);
-    agregar_abb(&root, 2);
+    agregar_abb(root, 3);
+    agregar_abb(root, 2);
     struct nodo_abb* nodoABorrar = agregar_abb(&root, 4);
-    agregar_abb(&root, 6);
-    agregar_abb(&root, 7);
-    agregar_abb(&root, 8);
+    agregar_abb(root, 6);
+    agregar_abb(root, 7);
+    agregar_abb(root, 8);
     
     mostrar_abb(root);
     printf("\r\n");
@@ -45,23 +45,25 @@ int main(int argc, char** argv) {
     return (EXIT_SUCCESS);
 }
 
-struct nodo_abb* agregar_abb(struct nodo_abb** arbol, int val) {
-    if ((*arbol) == NULL) {
-        (*arbol) = malloc(sizeof (struct nodo_abb));
-        (*arbol)->valor = val;
-        (*arbol)->cantidad = 1;
-        (*arbol)->der = NULL;
-        (*arbol)->izq = NULL;
-        return *arbol;
+struct nodo_abb* agregar_abb(struct nodo_abb* arbol, int val) {
+    
+    if (arbol == NULL) {
+        arbol = malloc(sizeof (struct nodo_abb));
+        arbol->valor = val;
+        arbol->cantidad = 1;
+        arbol->der = NULL;
+        arbol->izq = NULL;
+        return arbol;
     } else {
-        int valor = (*arbol)->valor;
+        int valor = arbol->valor;
         if (val == valor) {
-            (*arbol)->cantidad++;
-            return *arbol;
+            arbol->cantidad++;
+            return arbol;
         } else if (val < valor) {
-            return agregar_abb(&((*arbol)->izq), val);
+            return agregar_abb(arbol->izq, val);
+            
         } else {
-            return agregar_abb(&((*arbol)->der), val);
+            return agregar_abb(arbol->der, val);
         }
     }
 }
