44d8aeea3bcb11fa38319196708a07ba29c3408a
andiblas <andy_blas@hotmail.com>
2018-05-09 20:50:56 -0300

    lectura de parametros

--- main.c
+++ main.c
@@ -8,7 +8,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct nodo_abb {
+struct nodo_abb
+{
     int valor;
     int cantidad;
     struct nodo_abb *izq;
@@ -16,27 +17,26 @@ struct nodo_abb {
 };
 
 // FUNCIONES EN ASSEMBLER
-struct nodo_abb* agregar_abb(struct nodo_abb *arbol, int val);
+struct nodo_abb *agregar_abb(struct nodo_abb *arbol, int val);
 void borrar_abb(struct nodo_abb *a);
 void mostrar_abb(struct nodo_abb *a);
 
-
-struct nodo_abb* crear_nodo(int val);
-struct nodo_abb* crear_subnodo(struct nodo_abb** puntero, int val);
-struct nodo_abb* crear_subnodo_izq(struct nodo_abb* padre, int val);
-struct nodo_abb* crear_subnodo_der(struct nodo_abb* padre, int val);
-void mostrar_dfs(struct nodo_abb* arbol);
+struct nodo_abb *crear_nodo(int val);
+struct nodo_abb *crear_subnodo(struct nodo_abb **puntero, int val);
+struct nodo_abb *crear_subnodo_izq(struct nodo_abb *padre, int val);
+struct nodo_abb *crear_subnodo_der(struct nodo_abb *padre, int val);
+void mostrar_dfs(struct nodo_abb *arbol);
 
 struct nodo_abb *root;
 
-/*
- *
- */
-int main(int argc, char** argv) {
-    root = crear_nodo(5);
 
-    for (int i = 1; i < argc; i++) {
-      agregar_abb(root, atoi(argv[i]));
+int main(int argc, char **argv)
+{
+    root = crear_nodo(atoi(argv[1]));
+
+    for (int i = 2; i < argc; i++)
+    {
+        agregar_abb(root, atoi(argv[i]));
     }
 
     mostrar_abb(root);
@@ -44,9 +44,56 @@ int main(int argc, char** argv) {
     return (EXIT_SUCCESS);
 }
 
+struct nodo_abb *crear_subnodo_izq(struct nodo_abb *padre, int val)
+{
+    return crear_subnodo(&padre->izq, val);
+}
 
+struct nodo_abb *crear_subnodo_der(struct nodo_abb *padre, int val)
+{
+    return crear_subnodo(&padre->der, val);
+}
+
+struct nodo_abb *crear_subnodo(struct nodo_abb **puntero, int val)
+{
+    if (*puntero == NULL)
+    {
+        *puntero = crear_nodo(val);
+        return *puntero;
+    }
+    else
+    {
+        return agregar_abb(*puntero, val);
+    }
+}
+
+struct nodo_abb *crear_nodo(int val)
+{
+    struct nodo_abb *ret = malloc(sizeof(struct nodo_abb));
+    ret->valor = val;
+    ret->cantidad = 1;
+    ret->der = NULL;
+    ret->izq = NULL;
+    return ret;
+}
+
+void mostrar_dfs(struct nodo_abb *arbol)
+{
+    if (arbol->izq != NULL)
+    {
+        mostrar_dfs(arbol->izq);
+    }
+    printf("\n%d", arbol->valor);
+    if (arbol->der != NULL)
+    {
+        mostrar_dfs(arbol->der);
+    }
+}
 
 /*
+
+FUNCIONES IMPLEMENTADAS EN ASSEMBLER
+
 struct nodo_abb* agregar_abb(struct nodo_abb* arbol, int val) {
     int valor = arbol->valor;
     if (val == valor) {
@@ -59,45 +106,7 @@ struct nodo_abb* agregar_abb(struct nodo_abb* arbol, int val) {
     }
     return arbol;
 }
- */
 
-struct nodo_abb* crear_subnodo_izq(struct nodo_abb* padre, int val) {
-    return crear_subnodo(&padre->izq, val);
-}
-
-struct nodo_abb* crear_subnodo_der(struct nodo_abb* padre, int val) {
-    return crear_subnodo(&padre->der, val);
-}
-
-struct nodo_abb* crear_subnodo(struct nodo_abb** puntero, int val) {
-    if (*puntero == NULL) {
-        *puntero = crear_nodo(val);
-        return *puntero;
-    } else {
-        return agregar_abb(*puntero, val);
-    }
-}
-
-struct nodo_abb* crear_nodo(int val) {
-    struct nodo_abb* ret = malloc(sizeof (struct nodo_abb));
-    ret->valor = val;
-    ret->cantidad = 1;
-    ret->der = NULL;
-    ret->izq = NULL;
-    return ret;
-}
-
-void mostrar_dfs(struct nodo_abb* arbol) {
-  if (arbol->izq != NULL) {
-    mostrar_dfs(arbol->izq);
-  }
-  printf("\n%d", arbol->valor);
-  if (arbol->der != NULL) {
-    mostrar_dfs(arbol->der);
-  }
-}
-
-/*
 void mostrar_abb(struct nodo_abb* a) {
     if (a != NULL) {
         printf("{%d:%d", a->valor, a->cantidad);
@@ -106,9 +115,7 @@ void mostrar_abb(struct nodo_abb* a) {
         printf("}");
     }
 }
- */
 
-/*
 void borrar_abb(struct nodo_abb* a) {
     if (a != NULL) {
         borrar_abb(a->izq);
@@ -116,4 +123,5 @@ void borrar_abb(struct nodo_abb* a) {
         free(a);
     }
 }
- */
+
+*/
